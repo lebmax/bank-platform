02cc1ab66290290bdf3bf9f6495b1dff68f026ed
forever <user@forever>
2026-07-11 21:39:02 +0300

    review finish

--- notification-service/src/main/java/ru/ugaforever/bank/notification/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/ru/ugaforever/bank/notification/consumer/NotificationConsumer.java
@@ -1,7 +1,6 @@
 package ru.ugaforever.bank.notification.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.RecordMetadata;
